e1b651b52649ad04a722d883d834d97b93325b1e
Francisco Chardin <francisco.chardin@gmail.com>
2021-08-14 11:27:30 -0300

    primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Rick & Morty</h1>
+    <FilterByName />
+    <FilterByStatus />
+    <ListCharacters />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FilterByName from '@/components/FilterByName.vue'
+import FilterByStatus from '@/components/FilterByStatus'
+import ListCharacters from '@/components/ListCharacters'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FilterByName,
+    FilterByStatus,
+    ListCharacters
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+:root {
+  --background-body: #24282f;
+  --background-card: #3e3c44;
+  --text-white: #fff;
+  --text-gray: #c0c0c0;
+  --text-orange: #ff9800;
+}
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: var(--background-body);
+  color: var(--text-white);
+  font-family: 'Roboto', 'Tahoma', 'Verdana', sans-serif;
+}
+h1 {
+  margin-bottom: 3rem;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.container {
+  width: 980px;
+  max-width: 90%;
+  margin: 5rem auto;
+}
+img {
+  width: 100%;
 }
 </style>
---
+++ src/components/FilterByName.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="search">
+      <input 
+        type="text"
+        placeholder="Search by name"
+        v-model="name"
+        @keyup="filter()">
+      </div>
+</template>
+
+<script>
+//El ref nos permite utilizar el name en un v-model
+import { ref } from 'vue'
+import { useStore } from 'vuex'
+export default {
+    setup() {
+        const store = useStore();
+        const name = ref('')
+
+        const filter = () => {
+            store.dispatch('filterByName', name.value)
+        }
+        return {
+            name, 
+            filter
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.search {
+    width: 400px;
+    margin: 3rem auto 0;
+    input {
+        height: 53px;
+        width: 400px;
+        border: none;
+        border-radius: 10px;
+        padding: 0 0.5rem;
+        margin: 1rem 0;
+    }
+}
+</style>
